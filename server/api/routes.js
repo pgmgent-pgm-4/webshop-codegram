e4f707b19ad5eb01dbc03aa3fd01b29c8baabf51
Charlotte Delvaux <chardelv1@student.arteveldehs.be>
2021-05-10 15:28:49 +0200

    Add .sequelizerc

--- server/api/routes.js
+++ server/api/routes.js
@@ -6,6 +6,7 @@ import express from 'express';
 /**
  * Import custom packages
  */
+import { db } from '../db/models/index.js';
 // controllers i.e.: import * as parseUsers from './controllers/parseUsers.js';
 // middleware i.e.: import { isUserAuthenticated } from './middleware/auth.js';
 
@@ -23,7 +24,17 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).send('<h1>Welcome to the API!</h1>')
-})
+});
+
+router.get('/api/users', (req, res) => {
+  return db.User.findAll()
+    .then((users) => res.send(users))
+    .catch((err) => {
+      console.error('There was an error querying users', JSON.stringify(err))
+      return res.send(err)
+    });
+});
+
 
 /**
  * Export the router
