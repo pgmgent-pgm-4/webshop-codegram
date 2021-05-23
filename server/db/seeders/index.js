cf1937dc63565795997524dc868ac77210f8070b
Charlotte Delvaux <charlotte.delvaux@vauxdell.be>
2021-05-23 20:56:42 +0200

    Add a seedUsers function and working seeder script

--- server/db/seeders/index.js
+++ server/db/seeders/index.js
@@ -1,1 +1,38 @@
-import categoryController from '../../api/controllers/category.controller';
+// import 'babel-polyfill';
+import faker from 'faker';
+import { v4  as uuidv4 } from 'uuid';
+import database from '../index.js';
+
+database.connect();
+
+const generateUsers = (amount = 50) => {
+  let users = [];
+  for (let i = 0; i < amount; i++) {
+    let random = Math.floor(Math.random()*2);
+    const user = {
+      id: uuidv4(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      email_verified: !!random,
+      password: 'chinchillas4ever',
+      role: 'guest',
+      last_login: faker.date.recent(),
+    }
+    users.push(user);
+  }
+  return users;
+}
+
+const seedUsers = async () => {
+  try {
+    const generatedUsers = generateUsers();
+    generatedUsers.forEach(async (user) => {
+      await database.User.create(user)
+    });
+    console.table(generatedUsers)
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+seedUsers();
--- server/db/seeders/seed.users.js
+++ server/db/seeders/seed.users.js
@@ -1,17 +1,20 @@
+// import 'babel-polyfill';
 import faker from 'faker';
 import { v4  as uuidv4 } from 'uuid';
-import database from '../models/index.cjs';
+import database from '../index.js';
+
+database.connect();
 
 const generateUsers = (amount = 50) => {
   let users = [];
   for (let i = 0; i < amount; i++) {
-    let random = Math.round(Math.random);
+    let random = Math.floor(Math.random()*2);
     const user = {
       id: uuidv4(),
       username: faker.internet.userName(),
       email: faker.internet.email(),
-      email_verification: !!random,
-      password: faker.internet.password(),
+      email_verified: !!random,
+      password: 'chinchillas4ever',
       role: 'guest',
       last_login: faker.date.recent(),
     }
@@ -23,10 +26,13 @@ const generateUsers = (amount = 50) => {
 const seedUsers = async () => {
   try {
     const generatedUsers = generateUsers();
-    const users = await database.User.bulkCreate(generatedUsers);
-    console.table(users)
+    generatedUsers.forEach(async (user) => {
+      await database.User.create(user)
+    });
+    console.table(generatedUsers)
   } catch (err) {
     console.error(err);
   }
 }
 
+seedUsers();
