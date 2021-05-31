05585151fa26b7538ecbe7f2cc63ab06e752a421
pgm-chardelv1 <chardelv1@student.arteveldehs.be>
2021-05-31 19:45:09 +0200

    Add logout endpoint

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -15,3 +15,10 @@ dotenv.config();
 export const hashPassword = (password) => {
   return bcrypt.hashSync(password, parseInt(process.env.SALT_ROUNDS));
 };
+
+
+// Use local storage as a json data cache
+const writeToCache = (key, data) => Window.sessionStorage.setItem(key, JSON.stringify(data));
+const readFromCache = (key) => JSON.parse(Window.sessionStorage.getItem(key)) || null;
+
+export { readFromCache, writeToCache };
